8d0046c879403f83c60d84b0c0d1dc2c8a2c23a5
0xberkay <70816926+0xberkay@users.noreply.github.com>
2021-11-09 17:50:36 +0300

    main router

--- main.go
+++ main.go
@@ -17,14 +17,20 @@ func makine() {
 	var a int
 	fmt.Scan(&a)
 	if a == 1 {
-		fmt.Println("1. Çalıştırılmıştır.")
+		fmt.Println("1. ÇalıştırBağlıListe.")
 		ÇalıştırBağlıListe()
 
 	} else if a == 2 {
-		fmt.Println("2. Çalıştırılmıştır.")
+		fmt.Println("2. ÇifteListeÇalıştır.")
 		ÇifteListeÇalıştır()
 	} else if a == 3 {
-		fmt.Println("3. Çalıştırılmıştır.")
+		fmt.Println("3. ÇalıştırYığınlar.")
 		ÇalıştırYığınlar()
+	} else if a == 4 {
+		fmt.Println("4. ÇalıştırKuyruk.")
+		ÇalıştırKuyruk()
+	} else if a == 5 {
+		fmt.Println("5. DaireselListeÇalıştır.")
+		DaireselListeÇalıştır()
 	}
 }
